e749f9d45531954e303c83c840d955d3cff400ab
JefersonLRS <contato.jefersonlrs@gmail.com>
2024-04-19 13:42:32 -0300

    feat: Login page

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function signup () {
+    return (
+        <>
+        <Head>
+            <title>DevGourmet - Faça seu Cadastro</title>
+        </Head>
+        <main>
+            <h1>TESTE</h1>
+        </main>
+        </>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,14 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "purple-gourmet": "#811FFF",
+        "yellow-gourmet": "#FFDB1F",
+        "red-gourmet": "#DF2B2B",
+        "input-color": "#F3F3F3",
+      }
     },
   },
   plugins: [],
